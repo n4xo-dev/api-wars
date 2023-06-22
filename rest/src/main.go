911d8f3b47635106d61b4ce7e51cf10825a66d42
iLopezosa <ilopezosaserrano@gmail.com>
2023-06-22 20:53:42 +0200

    tables created

--- rest/src/main.go
+++ rest/src/main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/iLopezosa/api-wars/rest/src/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+func resetDB(db *gorm.DB) {
+	db.Migrator().DropTable(&models.User{}, &models.Post{}, &models.Comment{}, &models.Message{}, &models.Chat{})
+	db.AutoMigrate(&models.User{}, &models.Post{}, &models.Comment{}, &models.Message{}, &models.Chat{})
+
+}
 
 func main() {
 	fmt.Println("Creating connection to the database...")
 
-	db := dbConnect()
+	// db := dbConnect()
+	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable TimeZone=Europe/Madrid"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
 
 	fmt.Println("Resetting the database...")
 
