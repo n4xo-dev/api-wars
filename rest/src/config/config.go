0cbe2154ee97475d79aff1eaab5c5cda86fde29e
iLopezosa <ilopezosaserrano@gmail.com>
2023-06-26 23:08:53 +0200

    config once

--- rest/src/config/config.go
+++ rest/src/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"os"
 	"strconv"
+	"sync"
 )
 
 type Config struct {
@@ -15,39 +16,39 @@ type Config struct {
 
 var config Config
 
+var configOnce sync.Once
+
 func GetConfig() Config {
-	if config != (Config{}) {
-		return config
-	}
+	configOnce.Do(func() {
+		numOfUsers, err := strconv.Atoi(os.Getenv("NUM_OF_USERS"))
+		if err != nil {
+			numOfUsers = 5
+		}
+		numOfChats, err := strconv.Atoi(os.Getenv("NUM_OF_CHATS"))
+		if err != nil {
+			numOfChats = 3
+		}
+		numOfPosts, err := strconv.Atoi(os.Getenv("NUM_OF_POSTS"))
+		if err != nil {
+			numOfPosts = 3
+		}
+		maxNumOfMessages, err := strconv.Atoi(os.Getenv("MAX_NUM_OF_MESSAGES"))
+		if err != nil {
+			maxNumOfMessages = 5
+		}
+		maxNumOfParticipants, err := strconv.Atoi(os.Getenv("MAX_NUM_OF_PARTICIPANTS"))
+		if err != nil {
+			maxNumOfParticipants = 4
+		}
 
-	numOfUsers, err := strconv.Atoi(os.Getenv("NUM_OF_USERS"))
-	if err != nil {
-		numOfUsers = 5
-	}
-	numOfChats, err := strconv.Atoi(os.Getenv("NUM_OF_CHATS"))
-	if err != nil {
-		numOfChats = 3
-	}
-	numOfPosts, err := strconv.Atoi(os.Getenv("NUM_OF_POSTS"))
-	if err != nil {
-		numOfPosts = 3
-	}
-	maxNumOfMessages, err := strconv.Atoi(os.Getenv("MAX_NUM_OF_MESSAGES"))
-	if err != nil {
-		maxNumOfMessages = 5
-	}
-	maxNumOfParticipants, err := strconv.Atoi(os.Getenv("MAX_NUM_OF_PARTICIPANTS"))
-	if err != nil {
-		maxNumOfParticipants = 4
-	}
-
-	config = Config{
-		NumOfUsers:           numOfUsers,
-		NumOfChats:           numOfChats,
-		NumOfPosts:           numOfPosts,
-		MaxNumOfMessages:     maxNumOfMessages,
-		MaxNumOfParticipants: maxNumOfParticipants,
-	}
+		config = Config{
+			NumOfUsers:           numOfUsers,
+			NumOfChats:           numOfChats,
+			NumOfPosts:           numOfPosts,
+			MaxNumOfMessages:     maxNumOfMessages,
+			MaxNumOfParticipants: maxNumOfParticipants,
+		}
+	})
 
 	return config
 }
