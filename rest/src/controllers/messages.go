d6147f77c9d7f46b042121e968a94b481ef7f45a
iLopezosa <ilopezosaserrano@gmail.com>
2023-07-30 15:59:06 +0200

    Complete Message Read endpoints

--- rest/src/controllers/messages.go
+++ rest/src/controllers/messages.go
@@ -1,12 +1,54 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"errors"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/iLopezosa/api-wars/rest/src/db"
+	"gorm.io/gorm"
+)
 
 func MessageList(c *fiber.Ctx) error {
-	return c.SendStatus(501)
+	messages, err := db.MessageList()
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	} else {
+		return c.JSON(messages)
+	}
 }
 func MessageRead(c *fiber.Ctx) error {
-	return c.SendStatus(501)
+	id, err := strconv.ParseUint(c.Params("id"), 10, 64)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"error": "id is required",
+		})
+	}
+
+	if id < 1 {
+		return c.Status(400).JSON(fiber.Map{
+			"error": "id must be greater than 0",
+		})
+	}
+
+	m, err := db.MessageRead(id)
+
+	if err == nil {
+		return c.JSON(m)
+	}
+
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return c.Status(404).JSON(fiber.Map{
+			"error": "message not found",
+		})
+	}
+
+	return c.Status(500).JSON(fiber.Map{
+		"error": err.Error(),
+	})
 }
 func MessageCreate(c *fiber.Ctx) error {
 	return c.SendStatus(501)
