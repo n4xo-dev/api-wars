196fdad8c9967b03d24c013284e6761bd556e865
iLopezosa <ilopezosaserrano@gmail.com>
2023-10-28 20:17:57 +0200

    Defer disconnect before connecting to others

--- rest/cmd/main.go
+++ rest/cmd/main.go
@@ -60,8 +60,8 @@ func main() {
 	fmt.Println("\nCreating connection to the databases...")
 
 	db.Connect()
-	db.RedisConnect()
 	defer db.Disconnect()
+	db.RedisConnect()
 	defer db.RedisDisconnect()
 
 	// Reset and seed the database
