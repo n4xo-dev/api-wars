ca1699ce2769346449929b45db865c9fc838bae9
Ignacio Lopezosa Serrano <ilopezosaserrano@gmail.com>
2024-06-02 16:14:39 +0200

    REST consumer scenario test: baseURL with protocol path

--- tests/rest/consumer.js
+++ tests/rest/consumer.js
@@ -16,41 +16,41 @@ export const options = {
 // about authoring k6 scripts.
 //
 export default function() {
-  const baseURL = 'http://localhost:8080';
+  const baseURL = 'http://localhost:8080/rest';
   const ACTION_SLEEP = 60;
   // Get users and pick one
-  const users = http.get(`${baseURL}/rest/users/`).json();
+  const users = http.get(`${baseURL}/users/`).json();
   const randomUserIndex = Math.floor(Math.random() * users.length);
   const randomUserId = users[randomUserIndex].id;
-  http.get(`${baseURL}/rest/users/${randomUserId}`);
+  http.get(`${baseURL}/users/${randomUserId}`);
   sleep(ACTION_SLEEP);
   // Get user's posts and pick one
-  const posts = http.get(`${baseURL}/rest/users/${randomUserId}/posts`).json();
+  const posts = http.get(`${baseURL}/users/${randomUserId}/posts`).json();
   const randomPostIndex = Math.floor(Math.random() * posts.length);
   const randomPostId = posts[randomPostIndex].id;
-  http.get(`${baseURL}/rest/posts/${randomPostId}`);
+  http.get(`${baseURL}/posts/${randomPostId}`);
   sleep(ACTION_SLEEP);
   // Get post's comments and pick one
-  const comments = http.get(`${baseURL}/rest/posts/${randomPostId}/comments`).json();
+  const comments = http.get(`${baseURL}/posts/${randomPostId}/comments`).json();
   const randomCommentIndex = Math.floor(Math.random() * comments.length);
   const randomCommentId = comments[randomCommentIndex].id;
-  http.get(`${baseURL}/rest/comments/${randomCommentId}`);
+  http.get(`${baseURL}/comments/${randomCommentId}`);
   sleep(ACTION_SLEEP);
   // Get chats and pick one
-  const chats = http.get(`${baseURL}/rest/chats`).json();
+  const chats = http.get(`${baseURL}/chats`).json();
   const randomChatIndex = Math.floor(Math.random() * chats.length);
   const randomChatId = chats[randomChatIndex].id;
-  const chat = http.get(`${baseURL}/rest/chats/${randomChatId}?eager=true`).json();
+  const chat = http.get(`${baseURL}/chats/${randomChatId}?eager=true`).json();
   sleep(ACTION_SLEEP);
   // Get messages in chat and pick one
-  const messages = http.get(`${baseURL}/rest/chats/${randomChatId}/messages`).json();
+  const messages = http.get(`${baseURL}/chats/${randomChatId}/messages`).json();
   const randomMessageIndex = Math.floor(Math.random() * messages.length);
   const randomMessageId = messages[randomMessageIndex].id;
-  http.get(`${baseURL}/rest/messages/${randomMessageId}`);
+  http.get(`${baseURL}/messages/${randomMessageId}`);
   sleep(ACTION_SLEEP);
   // Get messages from a specific user in chat
   const randomChatUserIndex = Math.floor(Math.random() * chat.participants.length);
   const randomChatUserId = chat.participants[randomChatUserIndex].id;
-  http.get(`${baseURL}/rest/chats/${randomChatId}/user/${randomChatUserId}/messages`);
+  http.get(`${baseURL}/chats/${randomChatId}/user/${randomChatUserId}/messages`);
   sleep(ACTION_SLEEP);
 }
