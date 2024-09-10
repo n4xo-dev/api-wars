19aad41a3c7986b2a048203ca7b6019c9808f6c2
iLopezosa <ilopezosaserrano@gmail.com>
2024-09-10 16:52:16 +0200

    .sample.env on root for tests and updated tests and results to restart server on each test

--- runTests.sh
+++ runTests.sh
@@ -13,36 +13,34 @@ for dir in "${directories[@]}"; do
     # Check if the Go main file exists
     go_file="$base_dir/cmd/main.go"
     if [ -f "$go_file" ]; then
-      echo "Running Go program: $go_file"
-      # Reset the database
-      go run "$go_file" -reset
-      # Run the Go Server
-      go run "$go_file" -server &
-      go_pid=$!
+      # Loop through all JS files in the current directory
+      for jsfile in "$dir"/*.js; do
+        echo "Running Go program: $go_file"
+        # Reset the database
+        go run "$go_file" -reset
+        # Run the Go Server
+        go run "$go_file" -server &
+        go_pid=$!
 
-      # Allow the Go server some time to start (adjust if needed)
-      sleep 3
+        # Allow the Go server some time to start (adjust if needed)
+        sleep 3
+        # Check if there are any JS files
+        if [ -f "$jsfile" ]; then
+          # Remove the extension from the file name
+          base_name=$(basename "$jsfile" .js)
+
+          echo "Running k6 on $jsfile"
+          # Run k6, output JSON to a file, and CLI output to a text file
+          k6 run "$jsfile" --out json="tests/$base_name.json" > "tests/$base_name.txt" 2>&1
+        fi
+        # Kill the Go program after k6 tests are run
+        if [ ! -z "$go_pid" ]; then
+          echo "Killing Go process with PID $go_pid"
+          kill -2 $go_pid
+        fi
+      done
     else
       echo "Go program not found in $base_dir/cmd"
-    fi
-
-    # Loop through all JS files in the current directory
-    for jsfile in "$dir"/*.js; do
-      # Check if there are any JS files
-      if [ -f "$jsfile" ]; then
-        # Remove the extension from the file name
-        base_name=$(basename "$jsfile" .js)
-
-        echo "Running k6 on $jsfile"
-        # Run k6, output JSON to a file, and CLI output to a text file
-        k6 run "$jsfile" --out json="tests/$base_name.json" > "tests/$base_name.txt" 2>&1
-      fi
-    done
-
-    # Kill the Go program after k6 tests are run
-    if [ ! -z "$go_pid" ]; then
-      echo "Killing Go process with PID $go_pid"
-      kill -2 $go_pid
     fi
   else
     echo "Directory $dir does not exist"
